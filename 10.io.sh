53a0d5b4244362647ed03a1b0e02377ed10212d2
Su Liang <fangyansu@163.com>
2018-01-02 11:04:05 +0800

    update 10.io.sh

--- 10.io.sh
+++ 10.io.sh
@@ -1,3 +1,24 @@
-#!/bin/bash.sh
+#!/bin/bash
 
+# clear users file and save users into it
+who > users
+
+# append some content to users
+echo "Hello World!" >> users
+
+# display content of users file
+cat users
+
+#input redirection
+wc -l users
+
+#this right part is just a input, so it does not disply the file name like the previous one
+wc -l < users
+
+#Here document
+wc -l << EOF
+Hello
+World
+www.lenovo.com
+EOF
 
